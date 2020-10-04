a45c8c74bfce5cdf5112372f5584989b06ef5c6a
Thaidan Goguen-Bogdanis <thaidan.goguenbogdanis@ontariotechu.ne>
2020-10-03 20:59:34 -0400

    Files for In Class 2
    README has info on where things go, though really they just stay in the same folders but in a Unity project. S to see debug log.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 INFR3110InClass2
+
+In-Class 2 by Thaidan Goguen-Bogdanis, 100706090
+Followed along with the code in class.
+Hit s to see the debug log to show that it works.
+
+The dll and the dll metafile (I'm not sure if you need the metafile) go into plugins.
+The .cs file and the .cs metafile go into _Scripts (again, not sure you need the metafile).
+
+This should work in any blank Unity project.
---
+++ _Scripts/PluginTester.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class PluginTester : MonoBehaviour
+{
+    [StructLayout(LayoutKind.Sequential)]
+    struct Vector2D
+    {
+        public float x;
+        public float y;
+    }
+
+    const string DLL_NAME = "INFR3110-DLL-InClass";
+
+    [DllImport(DLL_NAME)]
+    private static extern int GetID();
+
+
+    [DllImport(DLL_NAME)]
+    private static extern void SetID(int id);
+
+    [DllImport(DLL_NAME)]
+    private static extern Vector2D GetPosition();
+
+    [DllImport(DLL_NAME)]
+    private static extern void SetPosition(float x, float y);
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown((KeyCode.S)))
+        {
+            SetID(12);
+            SetPosition(4.25f, 1.17f);
+
+            Debug.Log(GetID());
+            Debug.Log(GetPosition().x);
+            Debug.Log(GetPosition().y);
+        }
+    }
+}
